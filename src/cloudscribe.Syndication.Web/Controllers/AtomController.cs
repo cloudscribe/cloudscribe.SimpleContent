81e5d6b08778aa72eb22d8ed67b331a28bd9a1e1
Joe Audette <joeaudette@users.noreply.github.com>
2016-04-08 14:36:09 -0400

    add some comments for unimplemented class

--- src/cloudscribe.Syndication.Web/Controllers/AtomController.cs
+++ src/cloudscribe.Syndication.Web/Controllers/AtomController.cs
@@ -4,12 +4,18 @@
 // Created:                 2016-04-01
 // Last Modified:           2016-04-01
 // 
+// TODO: implement? having used my best google fu it seems best practice is to only provide one feed
+// https://blog.superfeedr.com/feeds/rss/atom/best%20practice/feed-publishing-best-practices/
+// rather than both an rss feed and an atom feed
+// since the rss feed is working and is probably all I really need I'm leaving this
+// unimplemented for now. if someone else wants to implement it and send a pull request
+// that would be great
 
 namespace cloudscribe.Syndication.Web.Controllers
 {
     //http://www.atomenabled.org/developers/syndication/
 
-    public class AtomController
-    {
-    }
+    //public class AtomController
+    //{
+    //}
 }
