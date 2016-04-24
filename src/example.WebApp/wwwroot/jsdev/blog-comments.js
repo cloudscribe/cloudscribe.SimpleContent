ccf14b70fb04d8626e58787f68b6a49028863547
Joe Audette <joeaudette@users.noreply.github.com>
2016-04-24 07:35:34 -0400

    add missing scripts to jsdev

---
+++ src/example.WebApp/wwwroot/jsdev/blog-comments.js
@@ -0,0 +1,285 @@
+﻿/* globals NodeList, HTMLCollection */
+
+(function () {
+    var postId = null;
+
+    //#region Helpers
+
+    function objectToUrl(obj) {
+        var string = '';
+
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                string += prop + '=' + obj[prop].replace(/ /g, '+') + '&';
+            }
+        }
+
+        return string.substring(0, string.length - 1);
+    }
+
+    var AsynObject = AsynObject ? AsynObject : {};
+
+    AsynObject.ajax = function (url, callback) {
+        var ajaxRequest = AsynObject.getAjaxRequest(callback);
+        ajaxRequest.open("GET", url, true);
+        ajaxRequest.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        ajaxRequest.send(null);
+    };
+
+    AsynObject.postAjax = function (url, callback, data) {
+        var ajaxRequest = AsynObject.getAjaxRequest(callback);
+        ajaxRequest.open("POST", url, true);
+        ajaxRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        ajaxRequest.send(objectToUrl(data));
+    };
+
+    AsynObject.getAjaxRequest = function (callback) {
+
+        var ajaxRequest = new XMLHttpRequest();
+
+        ajaxRequest.onreadystatechange = function () {
+            if (ajaxRequest.readyState > 1 && ajaxRequest.status > 0) {
+                callback(ajaxRequest.readyState, ajaxRequest.status, ajaxRequest.responseText);
+            }
+        };
+
+        return ajaxRequest;
+    };
+
+    function hasClass(elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+
+    function addClass(elem, className) {
+        if (!hasClass(elem, className)) {
+            elem.className += ' ' + className;
+        }
+    }
+
+    function removeClass(elem, className) {
+        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        }
+    }
+
+    function toDOM(htmlString) {
+        var wrapper = document.createElement('div');
+        wrapper.innerHTML = htmlString;
+        return wrapper.children;
+    }
+
+    function getParentsByAttribute(element, attr, value) {
+        var arr = [];
+
+        while (element) {
+            element = element.parentNode;
+            if (element.hasAttribute(attr) && element.getAttribute(attr) === value) {
+                arr.push(element);
+            }
+            if (!element.parentNode.parentNode) {
+                break;
+            }
+        }
+
+        return arr;
+    }
+
+    Element.prototype.remove = function () {
+        this.parentElement.removeChild(this);
+    };
+
+    NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+        for (var i = 0, len = this.length; i < len; i++) {
+            if (this[i] && this[i].parentElement) {
+                this[i].parentElement.removeChild(this[i]);
+            }
+        }
+    };
+
+    function slide(thisObj, direction, callback) {
+        if (direction === "Up") {
+            thisObj.style.height = '0px';
+        } else {
+            var clone = thisObj.cloneNode(true);
+
+            clone.style.position = 'absolute';
+            clone.style.visibility = 'hidden';
+            clone.style.height = 'auto';
+
+            addClass(clone, 'slideClone col-md-6 col-md-offset-3');
+
+            document.body.appendChild(clone);
+
+            var slideClone = document.getElementsByClassName("slideClone")[0];
+            var newHeight = slideClone.clientHeight;
+
+            slideClone.remove();
+            thisObj.style.height = newHeight + 'px';
+            if (callback) {
+                setTimeout(function () {
+                    callback();
+                }, 500);
+            }
+        }
+    }
+
+    //#endregion
+
+    var endpoint = "/comment.ashx";
+
+    function deleteComment(commentId, element) {
+        var deleteEndpoint = document.querySelector("[itemprop=blogPost]").getAttribute("data-delete-comment-url");
+        if (confirm("Do you want to delete this comment?")) {
+            AsynObject.postAjax(deleteEndpoint, function (state, status) {
+                if (state === 4 && status === 200) {
+                    slide(element, "Up", function () {
+                        element.remove();
+                    });
+                    return;
+                } else if (status !== 200) {
+                    alert("Something went wrong. Please try again");
+                }
+            }, {
+                mode: "delete",
+                postId: postId,
+                commentId: commentId,
+                __RequestVerificationToken: document.querySelector("input[name=__RequestVerificationToken]").getAttribute("value")
+            });
+        }
+    }
+
+    function approveComment(commentId, element) {
+        var approveEndpoint = document.querySelector("[itemprop=blogPost]").getAttribute("data-approve-comment-url");
+        AsynObject.postAjax(approveEndpoint, function (state, status) {
+            if (state === 4 && status === 200) {                
+                element.remove();
+                return;
+            } else if (status !== 200) {
+                alert("Something went wrong. Please try again");
+            }
+        }, {
+            mode: "approve",
+            postId: postId,
+            commentId: commentId,
+            __RequestVerificationToken: document.querySelector("input[name=__RequestVerificationToken]").getAttribute("value")
+        });
+    }
+
+    function saveComment(name, email, website, content, captchaResult, callback) {
+
+        if (localStorage) {
+            localStorage.setItem("name", name);
+            localStorage.setItem("email", email);
+            localStorage.setItem("website", website);
+        }
+        endpoint = document.getElementById("commentform").getAttribute("data-blog-path");
+        AsynObject.postAjax(endpoint, function (state, status, data) {
+
+            var elemStatus = document.getElementById("status");
+            if (state === 4 && status === 200) {
+                elemStatus.innerHTML = "Your comment has been added";
+                removeClass(elemStatus, "alert-danger");
+                addClass(elemStatus, "alert-success");
+
+                document.getElementById("commentcontent").value = "";
+
+                var comment = toDOM(data)[0];
+                comment.style.height = "0px";
+                var elemComments = document.getElementById("comments");
+                elemComments.appendChild(comment);
+                slide(comment, "Down");
+                callback(true);
+
+                return;
+            } else if (status !== 200) {
+                addClass(elemStatus, "alert-danger");
+                elemStatus.innerText = "Unable to add comment";
+                callback(false);
+            }
+        }, {
+            mode: "save",
+            postId: postId,
+            name: name,
+            email: email,
+            website: website,
+            content: content,
+            "g-recaptcha-response" : captchaResult,
+            __RequestVerificationToken: document.querySelector("input[name=__RequestVerificationToken]").getAttribute("value")
+        });
+
+    }
+
+    function initialize() {
+        postId = document.querySelector("[itemprop=blogPost]").getAttribute("data-id");
+        endpoint = document.getElementById("commentform").getAttribute("data-blog-path");
+        var email = document.getElementById("commentemail");
+        var name = document.getElementById("commentname");
+        var website = document.getElementById("commenturl");
+        var content = document.getElementById("commentcontent");
+        var commentForm = document.getElementById("commentform");
+        var recaptcha;
+
+        var allComments = document.querySelectorAll("[itemprop=comment]");
+        for (var i = 0; i < allComments.length; ++i) {
+            allComments[i].style.height = allComments[i].clientHeight + 'px';
+        }
+
+        commentForm.onsubmit = function (e) {
+            e.preventDefault();
+            var button = e.target;
+            button.setAttribute("disabled", true);
+            var recpatchaValue = "";
+            recaptcha = document.getElementById("g-recaptcha-response");
+            if (recaptcha)
+            {
+                recpatchaValue = recaptcha.value;
+                if (recpatchaValue.length == 0) { return; }
+            }
+            
+            saveComment(name.value, email.value, website.value, content.value, recpatchaValue, function () {
+                button.removeAttribute("disabled");
+                if (recaptcha) {
+                    grecaptcha.reset();
+                }
+                
+            });
+        };
+
+        website.addEventListener("keyup", function (e) {
+            var w = e.target;
+            if (w.value.trim().length >= 4 && w.value.indexOf("http") === -1) {
+                w.value = "http://" + w.value;
+            }
+        });
+
+        window.addEventListener("click", function (e) {
+            var tag = e.target;
+
+            if (hasClass(tag, "deletecomment")) {
+                var comment = getParentsByAttribute(tag, "itemprop", "comment")[0];
+                deleteComment(comment.getAttribute("data-id"), comment);
+            }
+            if (hasClass(tag, "approvecomment")) {
+                var comment = getParentsByAttribute(tag, "itemprop", "comment")[0];
+                approveComment(comment.getAttribute("data-id"), tag);
+            }
+        });
+
+        if (localStorage) {
+            email.value = localStorage.getItem("email");
+            website.value = localStorage.getItem("website");
+
+            if (name.value.length === 0) {
+                name.value = localStorage.getItem("name");
+            }
+        }
+    }
+
+    if (document.getElementById("commentform")) {
+        initialize();
+    }
+})();
---
+++ src/example.WebApp/wwwroot/jsdev/content-editor.js
@@ -0,0 +1,411 @@
+﻿(function ($) {
+
+    // #region Helpers
+
+    function ConvertMarkupToValidXhtml(markup) {
+        var docImplementation = document.implementation;
+        var htmlDocument = docImplementation.createHTMLDocument("temp");
+        var xHtmlDocument = docImplementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+        var xhtmlBody = xHtmlDocument.createElementNS('http://www.w3.org/1999/xhtml', 'body');
+
+        htmlDocument.body.innerHTML = "<div>" + markup + "</div>";
+
+        xHtmlDocument.documentElement.appendChild(xhtmlBody);
+        xHtmlDocument.importNode(htmlDocument.body, true);
+        xhtmlBody.appendChild(htmlDocument.body.firstChild);
+
+        /<body.*?><div>([\s\S]*?)<\/div><\/body>/i.exec(xHtmlDocument.documentElement.innerHTML);
+        return RegExp.$1;
+    }
+
+    // #endregion
+
+    var contentId, editMode, currentSlug, supportsCategories, contentType,
+        txtTitle, txtDateTime, txtExcerpt, txtContent, txtMessage, txtImage, txtPageOrder, chkPublish,
+        editorBar, btnNew, btnEdit, btnDelete, btnSave, btnCancel, btnOuterToggle,
+        indexPath, categoryPath, savePath, deletePath, cancelEditPath
+
+    editContent = function () {
+        txtTitle.attr('contentEditable', true);
+        txtExcerpt.attr('contentEditable', true);
+        txtExcerpt.css({ minHeight: "100px" });
+        txtExcerpt.parent().css('display', 'block');
+        txtContent.wysiwyg({ hotKeys: {}, activeToolbarClass: "active" });
+        txtContent.css({ minHeight: "400px" });
+        if (editMode == "new") {
+            // TODO: localize
+            txtTitle.attr('placeholder', 'type your title here');
+            txtExcerpt.attr('placeholder', 'type your meta description here');
+            txtContent.attr('placeholder', 'type your main content here');
+            //txtTitle.focus();
+        } else {
+            //txtContent.focus();
+        }
+
+        
+        txtDateTime.datetimepicker({
+            debug:false,
+            widgetPositioning: { horizontal: 'left', vertical: 'bottom' },
+            keepOpen: true,
+            allowInputToggle:true
+        });
+        
+        btnNew.attr("disabled", true);
+        btnEdit.attr("disabled", true);
+        btnSave.removeAttr("disabled");
+        btnCancel.removeAttr("disabled");
+        
+
+        if (editMode == "new")
+        {
+            $("#liDelete").hide();
+        }
+        else
+        {
+            $("#liDelete").show();
+        }
+        
+        $("#liSave").show();
+        $("#liCancel").show();
+        $("#liPublished").show();
+
+        if (contentType == "Page") {
+            $("#liPageOrder").show();
+        }
+        else
+        {
+            $("#liPageOrder").hide();
+        }
+
+        $('#liNewItem').hide();
+        $('#liEdit').hide();
+
+        //txtTitle.keyup(function () {
+        //    if ($(this).val().length != 0)
+        //        btnSave.removeAttr("disabled");
+        //    else
+        //        btnSave.attr('disabled', true);
+        //})
+        //if (txtTitle.is(':empty'))
+        //{
+        //    txtTitle.keyup(function () {
+        //        btnSave.prop('disabled', this.value == "" ? true : false);
+        //    })
+        //}
+        
+        if (supportsCategories) { showCategoriesForEditing(); }
+
+        toggleSourceView();
+
+        $("#tools").fadeIn().css("display", "inline-block");
+    },
+    cancelEdit = function () {
+        if (editMode.length > 0) {
+            if (confirm("Do you want to leave this page?")) {
+                history.back();
+            }
+        } else
+        {  
+            window.location = cancelEditPath;
+        }
+    },
+    toggleSourceView = function () {
+        $(".source").bind("click", function () {
+            var self = $(this);
+            if (self.attr("data-cmd") === "source") {
+                self.attr("data-cmd", "design");
+                self.addClass("active");
+                txtContent.text(txtContent.html());
+            } else {
+                self.attr("data-cmd", "source");
+                self.removeClass("active");
+                txtContent.html(txtContent.text());
+            }
+        });
+    },
+    saveContent = function (e) {
+        if ($(".source").attr("data-cmd") === "design") {
+            $(".source").click();
+        }
+
+        txtContent.cleanHtml();
+
+        var parsedDOM;
+
+        /*  IE9 doesn't support text/html MimeType https://github.com/madskristensen/MiniBlog/issues/35
+        
+            parsedDOM = new DOMParser().parseFromString(txtContent.html(), 'text/html');
+            parsedDOM = new XMLSerializer().serializeToString(parsedDOM);
+
+            /<body>(.*)<\/body>/im.exec(parsedDOM);
+            parsedDOM = RegExp.$1;
+        
+        */
+
+        /* When its time to drop IE9 support toggle commented region with 
+           the following statement and ConvertMarkupToXhtml function */
+        parsedDOM = ConvertMarkupToValidXhtml(txtContent.html());
+
+        //alert(txtDateTime.val());
+        var pageSort = 0;
+        if (contentType == "Page") { pageSort = txtPageOrder.val(); }
+
+        $.post(savePath, {
+            id: contentId,
+            isPublished: chkPublish.prop("checked"),
+            title: txtTitle.text().trim(),
+            pubDate: txtDateTime.val(),
+            pageOrder:pageSort,
+            metaDescription: txtExcerpt.text().trim(),
+            content: parsedDOM,
+            categories: getCategories(),
+            __RequestVerificationToken: document.querySelector("input[name=__RequestVerificationToken]").getAttribute("value")
+        },null,"text")
+          .success(function (data) {
+              location.href = data;
+              return;
+          })
+          .fail(function (data) {
+              if (data.status === 409) {
+                  showMessage(false, "The title is already in use");
+              } else {
+                  showMessage(false, "Something bad happened. Server reported " + data.status + " " + data.statusText);
+              }
+          })
+        ;
+    },
+    deleteContent = function () {
+
+        if (confirm("Are you sure you want to delete this " + contentType + "?")) {
+            $.post(deletePath,
+                {
+                    id: contentId,
+                    __RequestVerificationToken: document.querySelector("input[name=__RequestVerificationToken]").getAttribute("value")
+                })
+                .success(function () { location.href = indexPath; })
+                .fail(function () { showMessage(false, "Something went wrong. Please try again"); });
+        }
+    },
+    showMessage = function (success, message) {
+        var className = success ? "alert-success" : "alert-error";
+        txtMessage.addClass(className);
+        txtMessage.text(message);
+        txtMessage.parent().fadeIn();
+
+        setTimeout(function () {
+            txtMessage.parent().fadeOut("slow", function () {
+                txtMessage.removeClass(className);
+            });
+        }, 4000);
+    },
+    getCategories = function () {
+        var categories = '';
+
+        if ($("#txtCategories").length > 0) {
+            categories = $("#txtCategories").val();
+        } else {
+            $("ul.categories li a").each(function (index, item) {
+                if (categories.length > 0) {
+                    categories += ",";
+                }
+                categories += $(item).html();
+            });
+        }
+        return categories;
+    },
+    showCategoriesForEditing = function () {
+        var firstItemPassed = false;
+        var categoriesString = getCategories();
+        $("ul.categories li").each(function (index, item) {
+            if (!firstItemPassed) {
+                firstItemPassed = true;
+            } else {
+                $(item).remove();
+            }
+        });
+        $("ul.categories").append("<li><input id='txtCategories' class='form-control' /></li>");
+        $("#txtCategories").val(categoriesString);
+        $("#txtCategories").attr('placeholder', 'type comma separated categories here');
+    },
+    showCategoriesForDisplay = function () {
+        if ($("#txtCategories").length > 0) {
+            var categoriesArray = $("#txtCategories").val().split(',');
+            $("#txtCategories").parent().remove();
+
+            $.each(categoriesArray, function (index, category) {
+                $("ul.categories").append(' <li itemprop="articleSection" title="'
+                    + category + '"> <a href="'
+                    + categoryPath
+                    + encodeURIComponent(category.toLowerCase()) + '">'
+                    + category
+                    + '</a> </li> ');
+            });
+        }
+    },
+    setToolbarCookie = function () {
+        $.cookie('editor-toggle', 'hide', { path: '/' }); // cookie exists means hide toolbar
+        //alert('cookie set');
+    },
+    clearToolbarCookie = function () {
+        $.removeCookie('editor-toggle', { path: '/' }); // cookie exists means hide toolbar
+        //alert('cookie removed');
+    },
+    toolBarCookieExists = function () {
+        var toggleCookie = $.cookie('editor-toggle');
+        if (toggleCookie) {
+           //alert('cookie exists');
+            return true;
+        } // cookie exists means show
+        //alert('no cookie');
+        return false;
+    },
+    toggleToolbar = function () {
+        var editorHidden = editorBar.hasClass("invisible");
+        if (editorHidden) {
+            // editor was hidden, show it
+            editorBar.removeClass("invisible");
+            toolbarHeight = editorBar.height();
+            editorBar.animate({ "top": mainNavHeight }, 500, "swing");
+            addToolBarPadding();
+            btnOuterToggle.hide();
+            if (!toolBarCookieExists()) { setToolbarCookie(); }  
+        }
+        else {
+            //editor was visible toggle it to invisible
+            editorBar.addClass("invisible");
+            removeToolbarPadding();
+            // show the button to get the toolbar back
+            btnOuterToggle.show();
+            btnOuterToggle.animate({ "top": mainNavHeight, right: "0px" }, 500, "swing");
+            clearToolbarCookie();
+        }
+    },
+    setInitialToggleState = function () {
+        if (toolBarCookieExists()) {
+            // show the toolbar
+            //alert('cookie exists');
+            toggleToolbar(); // initial state hidden
+
+        }
+        else { // no cookie so toolbar is hidden
+            btnOuterToggle.animate({ "top": mainNavHeight, right: "0px" }, 500, "swing");  
+        }
+    },
+    addToolBarPadding = function () {
+        toolbarHeight = editorBar.height();
+        var mainDiv = $(".body-content").first();
+        mainDiv.animate({ "padding-top": toolbarHeight + mainNavHeight }, 500, "swing");
+        //mainDiv.css("background-color", "pink");
+    },
+    removeToolbarPadding = function () {
+        $(".body-content").first().animate({ "padding-top": mainNavHeight }, 500, "swing");
+    }
+    ;
+
+    editorBar = $("#editor-toolbar");
+    contentType = $("#editor-toolbar").data("content-type");
+
+    if (contentType == "Page") {
+        contentId = $("article").first().attr("data-id");
+        txtTitle = $("#article-title");
+        txtPageOrder = $("#PageOrder");
+    }
+    else
+    {
+        contentId = $("[itemprop~='blogPost']").attr("data-id");
+        txtTitle = $("[itemprop~='blogPost'] [itemprop~='name']");
+    }
+    
+
+    
+    txtExcerpt = $("[itemprop~='description']");
+    txtContent = $("[itemprop~='articleBody']");
+    txtMessage = $("#editor-toolbar .alert");
+    txtImage = $("#editor-toolbar #txtImage");
+    txtDateTime = $("[itemprop~='datePublished']");
+
+    btnNew = $("#btnNew");
+    btnEdit = $("#btnEdit");
+    btnDelete = $("#btnDelete").bind("click", deleteContent);
+    btnSave = $("#btnSave").bind("click", saveContent);
+    btnCancel = $("#btnCancel").bind("click", cancelEdit);
+    btnOuterToggle = $('#editor-outer-toggle');
+    chkPublish = $("#published");
+    indexPath = $("#editor-toolbar").data("index-path");
+    categoryPath = $("#editor-toolbar").data("category-path");
+    savePath = $("#editor-toolbar").data("save-path");
+    deletePath = $("#editor-toolbar").data("delete-path");
+    cancelEditPath = $("#editor-toolbar").data("cancel-edit-path");
+    currentSlug = $("#editor-toolbar").data("current-slug");
+    supportsCategories = ($("#editor-toolbar").data("supports-categories")) == 'True';
+    editMode = $("#editor-toolbar").data("edit-mode");
+    /*
+    isNew = (location.search.indexOf("slug") == -1)
+        && (
+        (location.search.indexOf("new") != -1)
+        )
+    ;
+    */
+
+    //alert(contentId);
+    var mainNavHeight = $(".navbar-fixed-top").first().height();
+    var toolbarHeight = editorBar.height();
+    var tbOriginalHeight = editorBar.height();
+    
+    // different no slug initial state for blog vs page
+    if (contentType == "Post") {
+        //alert('hey');
+        if (currentSlug.length == 0) {
+            $('#liEdit').hide();
+        }
+       
+    } 
+   
+    $('#btnHide').click(function (event) {
+        event.preventDefault();
+        toggleToolbar();
+    });
+    btnOuterToggle.click(function (event) {
+        event.preventDefault();
+        toggleToolbar();
+    });
+
+    setInitialToggleState();
+    
+    
+    if (currentSlug.length == 0) {
+        
+    }
+    
+
+    $(document).keyup(function (e) {
+        if (!document.activeElement.isContentEditable) {
+            if (e.keyCode === 46) { // Delete key
+                deleteContent();
+            } else if (e.keyCode === 27) { // ESC key
+                cancelEdit();
+            }
+        }
+    });
+
+    $('.uploadimage').click(function (e) {
+        e.preventDefault();
+        $('#txtImage').click();
+    });
+
+    if (editMode == "new" || editMode == "edit") {
+        editContent();
+        //$("#ispublished").fadeIn();
+        //chkPublish[0].checked = true;
+   // } else if (location.search.indexOf("mode=edit") != -1)
+   // {
+   //     editContent();   
+    }
+    else if(contentType == "Page") {
+        if (currentSlug) { btnEdit.removeAttr("disabled"); }
+    }
+     
+    $(".dropdown-menu > input").click(function (e) {
+        e.stopPropagation();
+    });
+})(jQuery);
