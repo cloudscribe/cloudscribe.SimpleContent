47ed69ae8bd72377af273b8993ccbc0485158b0c
Joe Audette <joeaudette@users.noreply.github.com>
2016-05-24 08:13:44 -0400

    add toolbar section to themes

--- src/example.WebApp/Startup.cs
+++ src/example.WebApp/Startup.cs
@@ -32,7 +32,7 @@ namespace example.WebApp
             // this file name is ignored by gitignore
             // so you can create it and use on your local dev machine
             // remember last config source added wins if it has the same settings
-            //builder.AddJsonFile("appsettings.local.overrides.json", optional: true);
+            builder.AddJsonFile("appsettings.local.overrides.json", optional: true);
 
             if (env.IsDevelopment())
             {
