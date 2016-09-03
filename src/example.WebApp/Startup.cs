df26c268d0ffd32590aadffe714ed3f3cd624fa8
Joe Audette <joeaudette@users.noreply.github.com>
2016-09-03 15:39:27 -0400

    add request localization with en-GB

--- src/example.WebApp/Startup.cs
+++ src/example.WebApp/Startup.cs
@@ -14,6 +14,8 @@ using cloudscribe.SimpleContent.Models;
 using Microsoft.AspNetCore.DataProtection;
 using cloudscribe.Core.SimpleContent.Integration;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 namespace example.WebApp
 {
@@ -108,38 +110,40 @@ namespace example.WebApp
 
             services.AddLocalization(options => options.ResourcesPath = "GlobalResources");
 
-            //services.Configure<RequestLocalizationOptions>(options =>
-            //{
-            //    var supportedCultures = new[]
-            //    {
-            //        new CultureInfo("en-US"),
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                var supportedCultures = new[]
+                {
+                    new CultureInfo("en-US"),
+                    new CultureInfo("en-GB"),
+                    new CultureInfo("fr-FR"),
+                    new CultureInfo("fr"),
+                };
 
-            //    };
+                // State what the default culture for your application is. This will be used if no specific culture
+                // can be determined for a given request.
+                options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
 
-            //    // State what the default culture for your application is. This will be used if no specific culture
-            //    // can be determined for a given request.
-            //    options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+                // You must explicitly state which cultures your application supports.
+                // These are the cultures the app supports for formatting numbers, dates, etc.
+                options.SupportedCultures = supportedCultures;
 
-            //    // You must explicitly state which cultures your application supports.
-            //    // These are the cultures the app supports for formatting numbers, dates, etc.
-            //    options.SupportedCultures = supportedCultures;
+                // These are the cultures the app supports for UI strings, i.e. we have localized resources for.
+                options.SupportedUICultures = supportedCultures;
 
-            //    // These are the cultures the app supports for UI strings, i.e. we have localized resources for.
-            //    options.SupportedUICultures = supportedCultures;
-
-            //    // You can change which providers are configured to determine the culture for requests, or even add a custom
-            //    // provider with your own logic. The providers will be asked in order to provide a culture for each request,
-            //    // and the first to provide a non-null result that is in the configured supported cultures list will be used.
-            //    // By default, the following built-in providers are configured:
-            //    // - QueryStringRequestCultureProvider, sets culture via "culture" and "ui-culture" query string values, useful for testing
-            //    // - CookieRequestCultureProvider, sets culture via "ASPNET_CULTURE" cookie
-            //    // - AcceptLanguageHeaderRequestCultureProvider, sets culture via the "Accept-Language" request header
-            //    //options.RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(async context =>
-            //    //{
-            //    //  // My custom request culture logic
-            //    //  return new ProviderCultureResult("en");
-            //    //}));
-            //});
+                // You can change which providers are configured to determine the culture for requests, or even add a custom
+                // provider with your own logic. The providers will be asked in order to provide a culture for each request,
+                // and the first to provide a non-null result that is in the configured supported cultures list will be used.
+                // By default, the following built-in providers are configured:
+                // - QueryStringRequestCultureProvider, sets culture via "culture" and "ui-culture" query string values, useful for testing
+                // - CookieRequestCultureProvider, sets culture via "ASPNET_CULTURE" cookie
+                // - AcceptLanguageHeaderRequestCultureProvider, sets culture via the "Accept-Language" request header
+                //options.RequestCultureProviders.Insert(0, new CustomRequestCultureProvider(async context =>
+                //{
+                //  // My custom request culture logic
+                //  return new ProviderCultureResult("en");
+                //}));
+            });
 
             services.Configure<MvcOptions>(options =>
             {
@@ -187,6 +191,7 @@ namespace example.WebApp
             ILoggerFactory loggerFactory,
             IOptions<cloudscribe.Core.Models.MultiTenantOptions> multiTenantOptionsAccessor,
             IServiceProvider serviceProvider,
+            IOptions<RequestLocalizationOptions> localizationOptionsAccessor,
             cloudscribe.Logging.Web.ILogRepository logRepo
             )
         {
@@ -211,6 +216,8 @@ namespace example.WebApp
             app.UseStatusCodePagesWithReExecute("/Home/Error/{0}");
 
             app.UseSession();
+
+            app.UseRequestLocalization(localizationOptionsAccessor.Value);
 
             app.UseMultitenancy<cloudscribe.Core.Models.SiteSettings>();
 
