56b35e95950d6ccf1b40eb1d69838b1a42428f99
Joe Audette <joeaudette@users.noreply.github.com>
2016-09-14 08:14:12 -0400

    add sample with entity framework

---
+++ samples/cloudscribecore-ef/src/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(ILogger<HomeController> logger)
+        {
+            log = logger;
+        }
+
+        private ILogger log;
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        public IActionResult Error(int statusCode)
+        {
+            if (statusCode == 404)
+            {
+                var statusFeature = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+                if (statusFeature != null)
+                {
+                    log.LogWarning("handled 404 for url: {OriginalPath}", statusFeature.OriginalPath);
+                }
+
+            }
+            return View(statusCode);
+        }
+    }
+}
