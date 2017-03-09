6b0ccbf2b3467c0ff3bf4c0695d023c1df6e0aaf
Joe Audette <joe.audette@gmail.com>
2017-03-09 12:30:04 -0500

    wip on disqus integration

---
+++ src/cloudscribe.SimpleContent.Web/RequestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace cloudscribe.SimpleContent.Web
+{
+    public static class RequestExtensions
+    {
+        public static string GetCurrentFullUrl(this HttpRequest request)
+        {
+
+            return request.Scheme + "://" + request.Host.ToUriComponent() + request.Path.Value;
+        }
+    }
+}
