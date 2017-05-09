e830657742964ff5f7e89eef1f82847196fd992d
Joe Audette <joe.audette@gmail.com>
2017-05-09 16:37:49 -0400

    wip on delete bug and deps update

--- src/cloudscribe.SimpleContent.Web/Services/HtmlProcessor.cs
+++ src/cloudscribe.SimpleContent.Web/Services/HtmlProcessor.cs
@@ -5,8 +5,10 @@ using System;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace cloudscribe.SimpleContent.Services
 {
@@ -297,6 +299,8 @@ namespace cloudscribe.SimpleContent.Services
             return new ImageSizeResult { Width=w, Height=h };
 
         }
+
+        
 
     }
 }
