fd540837c89d0fb2f315401762479e77b898654c
Joe Audette <joe.audette@gmail.com>
2018-01-24 16:49:45 -0500

    add failing test

--- test/cloudscribe.SimpleContent.Web.Tests/Services/TeaserServiceTests.cs
+++ test/cloudscribe.SimpleContent.Web.Tests/Services/TeaserServiceTests.cs
@@ -131,55 +131,79 @@ namespace cloudscribe.SimpleContent.Web.Services
         // this results in malformed html that breaks the read more link
         // double quotes changed manually here to single quotes
         // notice bad a link in output
-        string badInputHtml = @"<p>Once you get to know it</p>
+        string badInputHtml = @"<h3 id=""markdown-is-cool"">Markdown is cool!</h3>
+<p>Once you get to know it</p>
 <pre><code>private MarkdownPipeline _mdPipeline = null;
 
-    public string FilterHtml(IPage p)
-    {
-        if (p.ContentType == &quot;markdown&quot;)
+        public string FilterHtml(IPage p)
         {
-            if (_mdPipeline == null)
-            {
-                _mdPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            if (p.ContentType == &quot; markdown & quot;)
+        {
+                if (_mdPipeline == null)
+                {
+                    _mdPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                }
+                return Markdown.ToHtml(p.Content, _mdPipeline);
             }
-            return Markdown.ToHtml(p.Content, _mdPipeline);
-        }
 
-        return filter.FilterHtml(
-            p.Content,
-            ProjectSettings.CdnUrl,
-            ProjectSettings.LocalMediaVirtualPath);
-    }
+            return filter.FilterHtml(
+                p.Content,
+                ProjectSettings.CdnUrl,
+                ProjectSettings.LocalMediaVirtualPath);
+        }
 			
 </code></pre>
 <p>then add an image</p>
-<p><a href='/media/images/img_1349.jpg'><img src='/media/images/img_1349-ws.jpg' alt='my pond' /></a></p>";
+<p><a href=""/media/images/img_1349.jpg""><img src= ""/media/images/img_1349-ws.jpg"" alt= ""my pond"" /></a></p>";
 
 
-        string badOutputHtml = @"<p>Once you get to know it</p>
+        string badOutputHtml = @"<h3 id=""markdown-is-cool"">Markdown is cool!</h3>
+<p>Once you get to know it</p>
 <pre><code>private MarkdownPipeline _mdPipeline = null;
 
-    public string FilterHtml(IPage p)
-    {
-        if (p.ContentType == &quot;markdown&quot;)
+        public string FilterHtml(IPage p)
         {
-            if (_mdPipeline == null)
-            {
-                _mdPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            if (p.ContentType == &quot; markdown & quot;)
+        {
+                if (_mdPipeline == null)
+                {
+                    _mdPipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+                }
+                return Markdown.ToHtml(p.Content, _mdPipeline);
             }
-            return Markdown.ToHtml(p.Content, _mdPipeline);
-        }
 
-        return filter.FilterHtml(
-            p.Content,
-            ProjectSettings.CdnUrl,
-            ProjectSettings.LocalMediaVirtualPath);
-    }
+            return filter.FilterHtml(
+                p.Content,
+                ProjectSettings.CdnUrl,
+                ProjectSettings.LocalMediaVirtualPath);
+        }
 			
 </code></pre>
 <p>then add an image</p>
-<p></p><a href='/media/imag...href=''></a href='/media/imag...>
-                <a itemprop='url' href='/blog/2017/11/19/markdown-rocks'>[Read More]</a>";
+<p></p><a href=""/media/imag...";
+
+        [Fact]
+        [Description("If HTML tags would be cut off, make sure they are closed.")]
+        public void CreateTeaser_ShouldNotCreateBadOutput1()
+        {
+            postMock.Setup(m => m.TeaserOverride).Returns("");
+            postMock.Setup(m => m.SuppressAutoTeaser).Returns(false);
+            projectSettingsMock.Setup(m => m.AutoTeaserMode).Returns(AutoTeaserMode.On);
+            projectSettingsMock.Setup(m => m.TeaserTruncationMode).Returns(TeaserTruncationMode.Character);
+            projectSettingsMock.Setup(m => m.TeaserTruncationLength).Returns(400);
+
+            var post = postMock.Object;
+            var projectSettings = projectSettingsMock.Object;
+
+            var input = badInputHtml;
+            var expected = badOutputHtml + " href=\"\"></a href=\"/media/imag...>";
+            var result = teaserService.CreateTeaser(projectSettings, post, input);
+            bool bad = expected == result;
+            Assert.False(bad);
+        }
+
+        
+
 
 
     }
