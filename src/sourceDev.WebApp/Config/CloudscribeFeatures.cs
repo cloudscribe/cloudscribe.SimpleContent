94e83cdd4378a98d32a0bd9a182bb2d5d7d02394
Joe Audette <joe.audette@gmail.com>
2018-04-18 08:56:20 -0400

    tidy the startup

---
+++ src/sourceDev.WebApp/Config/CloudscribeFeatures.cs
@@ -0,0 +1,112 @@
+﻿using cloudscribe.SimpleContent.Models;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class CloudscribeFeatures
+    {
+        public static IServiceCollection SetupDataStorage(
+            this IServiceCollection services,
+            IConfiguration config
+            )
+        {
+            services.AddScoped<cloudscribe.Core.Models.Setup.ISetupTask, cloudscribe.Core.Web.Components.EnsureInitialDataSetupTask>();
+
+            var storage = config["DevOptions:DbPlatform"];
+            var efProvider = config["DevOptions:EFProvider"];
+
+            switch (storage)
+            {
+                case "NoDb":
+                    services.AddCloudscribeCoreNoDbStorage();
+                    // only needed if using cloudscribe logging with NoDb storage
+                    services.AddCloudscribeLoggingNoDbStorage(config);
+                    services.AddNoDbStorageForSimpleContent();
+
+                    break;
+
+                case "ef":
+                default:
+
+                    switch (efProvider)
+                    {
+                        case "sqlite":
+                            var slConnection = config.GetConnectionString("SQLiteEntityFrameworkConnectionString");
+                            services.AddCloudscribeCoreEFStorageSQLite(slConnection);
+                            services.AddCloudscribeLoggingEFStorageSQLite(slConnection);
+                            services.AddCloudscribeSimpleContentEFStorageSQLite(slConnection);
+
+                            break;
+
+                        case "pgsql":
+                            var pgConnection = config.GetConnectionString("PostgreSqlEntityFrameworkConnectionString");
+                            services.AddCloudscribeCoreEFStoragePostgreSql(pgConnection);
+                            services.AddCloudscribeLoggingEFStoragePostgreSql(pgConnection);
+                            services.AddCloudscribeSimpleContentEFStoragePostgreSql(pgConnection);
+
+                            break;
+
+                        case "MySql":
+                            var mysqlConnection = config.GetConnectionString("MySqlEntityFrameworkConnectionString");
+                            services.AddCloudscribeCoreEFStorageMySql(mysqlConnection);
+                            services.AddCloudscribeLoggingEFStorageMySQL(mysqlConnection);
+                            services.AddCloudscribeSimpleContentEFStorageMySQL(mysqlConnection);
+
+                            break;
+
+                        case "MSSQL":
+                        default:
+                            var connectionString = config.GetConnectionString("EntityFrameworkConnectionString");
+                            services.AddCloudscribeCoreEFStorageMSSQL(connectionString);
+
+                            // only needed if using cloudscribe logging with EF storage
+                            services.AddCloudscribeLoggingEFStorageMSSQL(connectionString);
+
+                            services.AddCloudscribeSimpleContentEFStorageMSSQL(connectionString);
+
+
+                            break;
+                    }
+
+
+
+
+                    break;
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection SetupCloudscribeFeatures(
+            this IServiceCollection services,
+            IConfiguration config
+            )
+        {
+            services.AddCloudscribeLogging();
+
+            //services.AddScoped<cloudscribe.Web.Navigation.Caching.ITreeCache, cloudscribe.Web.Navigation.Caching.NotCachedTreeCache>();
+
+            services.AddScoped<cloudscribe.Web.Navigation.INavigationNodePermissionResolver, cloudscribe.Web.Navigation.NavigationNodePermissionResolver>();
+            services.AddScoped<cloudscribe.Web.Navigation.INavigationNodePermissionResolver, cloudscribe.SimpleContent.Web.Services.PagesNavigationNodePermissionResolver>();
+
+            services.AddCloudscribeCoreMvc(config);
+
+            services.Configure<List<ProjectSettings>>(config.GetSection("ContentProjects"));
+
+            services.AddCloudscribeCoreIntegrationForSimpleContent(config);
+
+            // for testing that teasers can be disabled
+            //services.AddScoped<ITeaserService, cloudscribe.SimpleContent.Web.Services.TeaserServiceDisabled>();
+
+            services.AddSimpleContentMvc(config);
+
+            services.AddMetaWeblogForSimpleContent(config.GetSection("MetaWeblogApiOptions"));
+
+            services.AddSimpleContentRssSyndiction();
+
+            return services;
+        }
+
+    }
+}
---
+++ src/sourceDev.WebApp/Config/RoutingAndMvc.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class RoutingAndMvc
+    {
+        public static IRouteBuilder UseCustomRoutes(
+            this IRouteBuilder 
+            routes, bool useFolders,
+            IConfiguration config)
+        {
+            var useCustomRoutes = config.GetValue<bool>("DevOptions:UseCustomRoutes");
+
+            if (useFolders)
+            {
+                routes.AddBlogRoutesForSimpleContent(new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint());
+            }
+
+            routes.AddBlogRoutesForSimpleContent();
+            routes.AddSimpleContentStaticResourceRoutes();
+            routes.AddCloudscribeFileManagerRoutes();
+
+
+
+            if (useFolders)
+            {
+                routes.MapRoute(
+                   name: "foldererrorhandler",
+                   template: "{sitefolder}/oops/error/{statusCode?}",
+                   defaults: new { controller = "Oops", action = "Error" },
+                   constraints: new { name = new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint() }
+                );
+
+                if (useCustomRoutes)
+                {
+                    routes.MapRoute(
+                    name: "folderdefault",
+                    template: "{sitefolder}/{controller}/{action}/{id?}"
+                    , defaults: new { controller = "Home", action = "Index" }
+                    , constraints: new { name = new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint() }
+                    );
+
+                    routes.AddCustomPageRouteForSimpleContent(new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint(), "docs");
+                }
+                else
+                {
+                    routes.MapRoute(
+                        name: "foldersitemap",
+                        template: "{sitefolder}/sitemap"
+                        , defaults: new { controller = "Page", action = "SiteMap" }
+                        , constraints: new { name = new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint() }
+                        );
+
+                    routes.MapRoute(
+                    name: "folderdefault",
+                    template: "{sitefolder}/{controller}/{action}/{id?}"
+                    , defaults: new { controller = "Home" }
+                    , constraints: new { name = new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint() }
+                    );
+
+                    routes.AddDefaultPageRouteForSimpleContent(new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint());
+
+                    //routes.AddBlogRoutesForSimpleContent(new cloudscribe.Core.Web.Components.SiteFolderRouteConstraint(),"");
+                }
+
+
+            }
+
+            if (useCustomRoutes)
+            {
+                routes.AddCustomPageRouteForSimpleContent("docs");
+            }
+
+
+            routes.MapRoute(
+               name: "errorhandler",
+               template: "oops/error/{statusCode?}",
+               defaults: new { controller = "Oops", action = "Error" }
+               );
+
+            routes.MapRoute(
+                name: "sitemap",
+                template: "sitemap"
+                , defaults: new { controller = "Page", action = "SiteMap" }
+                );
+
+            var useHomeIndexAsDefault = config.GetValue<bool>("DevOptions:UseHomeIndexAsDefault");
+            if (useHomeIndexAsDefault)
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller}/{action}"
+                , defaults: new { controller = "Home", action = "Index" }
+                );
+            }
+            else
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller}/{action}"
+                //, defaults: new { controller = "Home", action = "Index" }
+                );
+            }
+
+
+            if (!useCustomRoutes)
+            {
+                routes.AddDefaultPageRouteForSimpleContent();
+            }
+            //routes.AddBlogRoutesForSimpleContent("");
+
+
+            return routes;
+        }
+
+
+        public static IServiceCollection SetupMvc(
+            this IServiceCollection services,
+            bool sslIsAvailable
+            )
+        {
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto;
+            });
+
+            services.Configure<MvcOptions>(options =>
+            {
+                if (sslIsAvailable)
+                {
+                    options.Filters.Add(new RequireHttpsAttribute());
+                }
+
+
+                options.CacheProfiles.Add("SiteMapCacheProfile",
+                     new CacheProfile
+                     {
+                         Duration = 30
+                     });
+
+                options.CacheProfiles.Add("RssCacheProfile",
+                     new CacheProfile
+                     {
+                         Duration = 100
+                     });
+            });
+
+            services.AddRouting(options =>
+            {
+                options.LowercaseUrls = true;
+            });
+
+            services.AddMvc()
+                .AddRazorOptions(options =>
+                {
+                    //options.AddCloudscribeViewLocationFormats();
+
+
+                    options.AddCloudscribeCommonEmbeddedViews();
+                    options.AddCloudscribeNavigationBootstrap3Views();
+                    options.AddCloudscribeCoreBootstrap3Views();
+                    options.AddCloudscribeFileManagerBootstrap3Views();
+                    options.AddCloudscribeSimpleContentBootstrap3Views();
+                    options.AddCloudscribeLoggingBootstrap3Views();
+
+                    options.ViewLocationExpanders.Add(new cloudscribe.Core.Web.Components.SiteViewLocationExpander());
+
+                });
+
+            return services;
+        }
+    }
+}
