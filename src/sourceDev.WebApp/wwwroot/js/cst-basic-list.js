abf1fffe27e31532682711ed3085c2c361b68534
Joe Audette <joe.audette@gmail.com>
2018-07-13 10:51:50 -0400

    wip on #292

--- src/sourceDev.WebApp/wwwroot/js/cst-basic-list.js
+++ src/sourceDev.WebApp/wwwroot/js/cst-basic-list.js
@@ -16,20 +16,58 @@ function ImageItem(title, description, fullSizeUrl, resizedUrl, thumbnailUrl, li
 function ItemListViewModel(initialData) {
     var self = this;
     self.hiddenField = document.getElementById("ItemsJson");
+
+    self.handleSortItemChanged = function (sortVal) {
+        console.log(sortVal);
+        self.sortItems();
+        //ko.cleanNode(document.getElementById("itemsEditList"))
+        //ko.applyBindings(self.Items, document.getElementById("itemsEditList"))
+
+        //self.Items.valueHasMutated();
+        //self.sortItems();
+        //var tmp = self.Items();
+        //self.Items([]);
+        //tmp.sort(function (a, b) {
+        //    if (a.Sort < b.Sort) { return -1; }
+        //    if (a.Sort > b.Sort) { return 1; }
+        //    return 0;
+        //});
+        //self.Items(tmp);
+
+    }
     
     self.Items = ko.observableArray(ko.utils.arrayMap(initialData, function (item) {
         //console.log(item);
-        return new ImageItem(item.Title, item.Description, item.ResizedUrl, item.FullSizeUrl, item.ThumbnailUrl, item.LinkUrl, item.Sort);
+        var item = new ImageItem(item.Title, item.Description, item.ResizedUrl, item.FullSizeUrl, item.ThumbnailUrl, item.LinkUrl, item.Sort);
+        item.Sort.subscribe(self.handleSortItemChanged);
+        return item;
     }));
 
     self.addItem = function (title, description, fullSizeUrl, resizedUrl, thumbnailUrl, linkUrl, sort) {
-        self.Items.push(new ImageItem(title, description, fullSizeUrl, resizedUrl, thumbnailUrl, linkUrl, sort))
+        var item = new ImageItem(title, description, fullSizeUrl, resizedUrl, thumbnailUrl, linkUrl, sort);
+        item.Sort.subscribe(self.handleSortItemChanged);
+        self.Items.push(item)
+    }
+
+    self.newItemFullSizeUrl = ko.observable(null);
+    self.newItemResizedUrl = ko.observable(null);
+    self.newItemThumbnailUrl = ko.observable(null);
+    //self.hasNewItem = ko.computed(function () {
+    //    return document.getElementById("imgUrlResized").value.length > 0;
+    //});
+
+    self.addNewItem = function () {
+        self.addItem('', '', self.newItemFullSizeUrl(), self.newItemResizedUrl(), self.newItemThumbnailUrl())
     }
 
     self.removeItem = function (item) { self.Items.remove(item) }
 
     self.test = function () {
         self.addItem('Fungus', 'Fungus amoung us', '/media/images/img_1150.jpg', '/media/images/img_1150-ws.jpg', '', '', 9);
+    }
+
+    self.test2 = function () {
+        console.log(self.newItemResizedUrl());
     }
 
     self.currentListState = ko.computed(function () {
