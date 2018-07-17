68db2a6ed2dda16d0c9b3e2e227ea610e85a8d8f
Joe Audette <joe.audette@gmail.com>
2018-07-17 11:00:43 -0400

    wip on #292

--- src/cloudscribe.SimpleContent.ContentTemplates.Bootstrap4/Services/ContentTemplateProvider.cs
+++ src/cloudscribe.SimpleContent.ContentTemplates.Bootstrap4/Services/ContentTemplateProvider.cs
@@ -5,6 +5,7 @@
 // 
 
 using cloudscribe.SimpleContent.Models;
+using Microsoft.Extensions.Localization;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -12,14 +13,17 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
 {
     public class ContentTemplateProvider : IContentTemplateProvider
     {
-        public ContentTemplateProvider()
+        public ContentTemplateProvider(
+            IStringLocalizer<ContentTemplateResources> stringLocalizer
+            )
         {
-
+            _sr = stringLocalizer;
         }
 
         private List<ContentTemplate> _list = null;
+        private IStringLocalizer _sr;
 
-        
+
 
         private void BuildList()
         {
@@ -30,7 +34,7 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
             _list.Add(BuildImageWithContent());
             _list.Add(BuildGalleryWithContent());
             //_list.Add(BuildListOfLinks());
-            _list.Add(BuildGoogleMap());
+            //_list.Add(BuildGoogleMap());
 
         }
 
@@ -49,8 +53,8 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
             var template = new ContentTemplate()
             {
                 Key = "sct-GalleryWithContent",
-                Title = "Simple image gallery with optional content above and below",
-                Description = "The gallery has 6 layouts to choose from: cards, compact, grid, carousel full width, carousel left with wrapped content, and carousel right with wrapped content.",
+                Title = _sr["Simple image gallery with optional content above and below"],
+                Description = _sr["The gallery has 6 layouts to choose from: cards, compact, grid, carousel full width, carousel left with wrapped content, and carousel right with wrapped content."],
                 EditView = "ContentTemplates/GalleryWithContentEdit",
                 RenderView = "ContentTemplates/GalleryWithContentRender",
                 ScreenshotUrl = "",
@@ -159,7 +163,7 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
             var template = new ContentTemplate()
             {
                 Key = "sct-ListOfLinks",
-                Title = "A list of links with optional thumbnail per link and content above and below",
+                Title = _sr["A list of links with optional thumbnail per link and content above and below"],
                 Description = "",
                 EditView = "ContentTemplates/ListOfLinksEdit",
                 RenderView = "ContentTemplates/ListOfLinksRender",
@@ -248,8 +252,8 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
             var template = new ContentTemplate()
             {
                 Key = "sct-ColumnsWithImages",
-                Title = "Columns with optional top images",
-                Description = "One to four column layout with an optional image at the top of each column.",
+                Title = _sr["Columns with optional top images"],
+                Description = _sr["One to four column responsive layout with an optional image at the top of each column."],
                 EditView = "ContentTemplates/ColumnWithImageEdit",
                 RenderView = "ContentTemplates/ColumnWithImageRender",
                 ModelType = "cloudscribe.SimpleContent.ContentTemplates.ViewModels.ColumnsWithImageViewModel, cloudscribe.SimpleContent.ContentTemplates.Bootstrap4",
@@ -324,8 +328,8 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
             var template = new ContentTemplate()
             {
                 Key = "sct-ImageWithContent",
-                Title = "Image with wrapped content",
-                Description = "A simple image floated left or right so that content wraps around it.",
+                Title = _sr["Image with wrapped content"],
+                Description = _sr["A simple image floated left or right so that content wraps around it."],
                 EditView = "ContentTemplates/ImageWithContentEdit",
                 RenderView = "ContentTemplates/ImageWithContentRender",
                 ModelType = "cloudscribe.SimpleContent.ContentTemplates.ViewModels.ImageWithContentViewModel, cloudscribe.SimpleContent.ContentTemplates.Bootstrap4",
@@ -406,7 +410,7 @@ namespace cloudscribe.SimpleContent.ContentTemplates.Services
             var template = new ContentTemplate()
             {
                 Key = "sct-googlemap",
-                Title = "Google Map with content above and below",
+                Title = _sr["Google Map with content above and below"],
                 Description = "",
                 EditView = "ContentTemplates/GoogleMapEdit",
                 RenderView = "ContentTemplates/GoogleMapRender",
