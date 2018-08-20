adb64e7260deaadbb2f385a99694588396c6029a
Joe Audette <joe.audette@gmail.com>
2018-08-20 15:24:17 -0400

    fix sqlite migration

--- src/cloudscribe.SimpleContent.Storage.EFCore.SQLite/Migrations/20180820133304_simplecontent-20180819.cs
+++ src/cloudscribe.SimpleContent.Storage.EFCore.SQLite/Migrations/20180820133304_simplecontent-20180819.cs
@@ -11,23 +11,23 @@ namespace cloudscribe.SimpleContent.Storage.EFCore.SQLite.Migrations
                 table: "cs_Post",
                 nullable: true);
 
-            migrationBuilder.AlterColumn<byte>(
-                name: "TeaserTruncationMode",
-                table: "cs_ContentProject",
-                nullable: false,
-                defaultValue: (byte)0,
-                oldClrType: typeof(byte),
-                oldDefaultValue: (byte)0)
-                .OldAnnotation("Sqlite:Autoincrement", true);
+            //migrationBuilder.AlterColumn<byte>(
+            //    name: "TeaserTruncationMode",
+            //    table: "cs_ContentProject",
+            //    nullable: false,
+            //    defaultValue: (byte)0,
+            //    oldClrType: typeof(byte),
+            //    oldDefaultValue: (byte)0)
+            //    .OldAnnotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<byte>(
-                name: "TeaserMode",
-                table: "cs_ContentProject",
-                nullable: false,
-                defaultValue: (byte)0,
-                oldClrType: typeof(byte),
-                oldDefaultValue: (byte)0)
-                .OldAnnotation("Sqlite:Autoincrement", true);
+            //migrationBuilder.AlterColumn<byte>(
+            //    name: "TeaserMode",
+            //    table: "cs_ContentProject",
+            //    nullable: false,
+            //    defaultValue: (byte)0,
+            //    oldClrType: typeof(byte),
+            //    oldDefaultValue: (byte)0)
+            //    .OldAnnotation("Sqlite:Autoincrement", true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -36,23 +36,23 @@ namespace cloudscribe.SimpleContent.Storage.EFCore.SQLite.Migrations
                 name: "AutoTeaser",
                 table: "cs_Post");
 
-            migrationBuilder.AlterColumn<byte>(
-                name: "TeaserTruncationMode",
-                table: "cs_ContentProject",
-                nullable: false,
-                defaultValue: (byte)0,
-                oldClrType: typeof(byte),
-                oldDefaultValue: (byte)0)
-                .Annotation("Sqlite:Autoincrement", true);
+            //migrationBuilder.AlterColumn<byte>(
+            //    name: "TeaserTruncationMode",
+            //    table: "cs_ContentProject",
+            //    nullable: false,
+            //    defaultValue: (byte)0,
+            //    oldClrType: typeof(byte),
+            //    oldDefaultValue: (byte)0)
+            //    .Annotation("Sqlite:Autoincrement", true);
 
-            migrationBuilder.AlterColumn<byte>(
-                name: "TeaserMode",
-                table: "cs_ContentProject",
-                nullable: false,
-                defaultValue: (byte)0,
-                oldClrType: typeof(byte),
-                oldDefaultValue: (byte)0)
-                .Annotation("Sqlite:Autoincrement", true);
+            //migrationBuilder.AlterColumn<byte>(
+            //    name: "TeaserMode",
+            //    table: "cs_ContentProject",
+            //    nullable: false,
+            //    defaultValue: (byte)0,
+            //    oldClrType: typeof(byte),
+            //    oldDefaultValue: (byte)0)
+            //    .Annotation("Sqlite:Autoincrement", true);
         }
     }
 }
