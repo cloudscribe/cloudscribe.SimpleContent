accacb4b3012c8e5fd0ea8eec66c18d73e8b3a75
Joe Audette <joeaudette@users.noreply.github.com>
2019-02-23 10:12:20 -0500

    update package deps

--- src/sourceDev.WebApp/Config/Authorization.cs
+++ src/sourceDev.WebApp/Config/Authorization.cs
@@ -21,6 +21,13 @@ namespace Microsoft.Extensions.DependencyInjection
                 });
 
             options.AddPolicy(
+                "FileUploadPolicy",
+                authBuilder =>
+                {
+                    authBuilder.RequireRole("Administrators", "Content Administrators");
+                });
+
+            options.AddPolicy(
                 "FileManagerDeletePolicy",
                 authBuilder =>
                 {
