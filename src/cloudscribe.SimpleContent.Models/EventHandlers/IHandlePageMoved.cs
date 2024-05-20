c7f5448116061849969d355bf693b4d63c036cee
Jim Kerslake <39943820+JimKerslake@users.noreply.github.com>
2024-05-20 10:00:06 +0100

    #591 page moved event handler

---
+++ src/cloudscribe.SimpleContent.Models/EventHandlers/IHandlePageMoved.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace cloudscribe.SimpleContent.Models.EventHandlers
+{
+    public interface IHandlePageMoved
+    {
+        Task Handle(
+            string projectId,
+            IPage movedPage,
+            IPage targetPage,
+            string position,
+            CancellationToken cancellationToken = default(CancellationToken)
+            );
+    }
+}
