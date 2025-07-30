25043140b39bbd7aca44f74f6c904be059f6e4b0
Jim Kerslake <39943820+JimKerslake@users.noreply.github.com>
2025-07-30 11:32:15 +0100

    #653 delete disused packages

--- src/cloudscribe.SimpleContent.Storage.EFCore.pgsql/StorageInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using cloudscribe.SimpleContent.Models;
-
-namespace cloudscribe.SimpleContent.Storage.EFCore.pgsql
-{
-    public class StorageInfo : IStorageInfo
-    {
-        public string StoragePlatform { get { return "Entity Framework with PostgreSql"; } }
-    }
-}
